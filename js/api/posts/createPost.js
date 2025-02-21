0590b4641759f359717da5c49936df5fed73f264
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-02-21 17:18:44 +0100

    create post

--- js/api/posts/createPost.js
+++ js/api/posts/createPost.js
@@ -1,5 +1,5 @@
 import { getToken } from "../../utils/localStorage.js";
-import { NOROFF_API_KEY, POSTS_URL } from "../../constants/api.js";
+import { NOROFF_API_KEY, POSTS_URL, TAG } from "../../constants/api.js";
 
 export async function createPost(post) {
   try {
@@ -19,7 +19,8 @@ export async function createPost(post) {
       body: JSON.stringify(post),
     };
 
-    const response = await fetch(POSTS_URL, options);
+    const TAG_URL = `${POSTS_URL}?_tag=${TAG}`;
+    const response = await fetch(TAG_URL, options);
     const json = await response.json();
     return json.data;
   } catch (error) {
--- js/constants/api.js
+++ js/constants/api.js
@@ -4,7 +4,7 @@ export const TAG = "theb00ks";
 
 export const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`;
 export const AUTH_LOGIN_URL = `${BASE_API_URL}/auth/login`;
-export const POSTS_URL = `${BASE_API_URL}/social/posts?_tag=${TAG}`;
+export const POSTS_URL = `${BASE_API_URL}/social/posts`;
 export const PROFILES_URL = `${BASE_API_URL}/social/profiles`;
 
 export const NOROFF_API_KEY = "2770d9d4-18cf-4ffa-ac12-2a5f76b67f29";
